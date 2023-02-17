64296b4d35f95a6d59a9f650fb4e87bd1561f0ef
Jahongir2109 <jahongirachilov004@gmail.com>
2023-02-17 10:50:33 +0500

    I believe that is finish all test

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,24 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Map<String, String> map=new HashMap<>();
+        String str;
+        try(InputStream inputStream=new FileInputStream(file)) {
+            BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream));
+            while (bufferedReader.ready()){
+                str=bufferedReader.readLine();
+                map.put(str.substring(0,str.indexOf(": ")),str.substring(str.indexOf(": ")+2));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new Profile(map.get("Name"),Integer.valueOf(map.get("Age")),map.get("Email"),Long.valueOf(map.get("Phone")));
     }
 }
